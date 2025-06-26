8d8b2cc51c4ce3039cc50797bfe56c57642618ed
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2025-06-26 19:42:29 +0200

    chore: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "A Nix-flake for the Busy-Bees development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+
+  outputs =
+    inputs:
+    let
+      nodejsVersion = 22;
+
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [ inputs.self.overlays.default ];
+            };
+          }
+        );
+    in
+    {
+      overlays.default = final: prev: {
+        nodejs = final."nodejs_${toString nodejsVersion}";
+      };
+
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              awscli2
+              nixfmt-rfc-style
+              nodejs
+            ];
+          };
+        }
+      );
+    };
+}
+
