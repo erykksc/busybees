53f0c1d50f92a7225ae80f82b791a847265b558b
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2025-07-11 16:12:17 +0200

    feat(api): implement api/groups/[groupId]/profile.GET

--- packages/functions/src/api/groups/[groupId]/profile.GET.ts
+++ packages/functions/src/api/groups/[groupId]/profile.GET.ts
@@ -1,18 +1,58 @@
 import { Logger } from "@aws-lambda-powertools/logger";
-import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { getGroupCalendar } from "@busybees/core";
+import {
+  APIGatewayProxyEventV2WithJWTAuthorizer,
+  APIGatewayProxyResultV2,
+} from "aws-lambda";
+
+const dbClient = DynamoDBDocumentClient.from(new DynamoDBClient({}));
 
 const logger = new Logger({
   serviceName: "sst-app",
 });
 
 export const main = async (
-  event: APIGatewayProxyEventV2,
+  event: APIGatewayProxyEventV2WithJWTAuthorizer,
 ): Promise<APIGatewayProxyResultV2> => {
   try {
+    const groupId = event.pathParameters?.groupId;
+
+    if (!groupId || typeof groupId !== "string") {
+      return {
+        statusCode: 400, // Bad Request
+        headers: {
+          ContentType: "application/json",
+        },
+        body: JSON.stringify({
+          error: "Invalid groupId in path parameters, must be a string",
+        }),
+      };
+    }
+
+    const groupCalendar = await getGroupCalendar(dbClient, {
+      groupId,
+    });
+    if (!groupCalendar) {
+      return {
+        statusCode: 404, // Not Found
+        headers: {
+          ContentType: "application/json",
+        },
+        body: JSON.stringify({
+          error: "Group calendar not found",
+        }),
+      };
+    }
+
     return {
-      statusCode: 501, // Not implementd
+      statusCode: 200, // OK
+      headers: {
+        ContentType: "application/json",
+      },
       body: JSON.stringify({
-        error: "The requested resource is not implemented yet.",
+        groupProfile: groupCalendar,
       }),
     };
   } catch (error) {
